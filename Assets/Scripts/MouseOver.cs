7fce9733dd3aaf3f56159ec76dcfc44b37675461
Cocoa Milka <59587889+CocoaMilka@users.noreply.github.com>
2023-06-27 14:37:12 -0500

    feat: Add ability to convert grass blocks to farmland

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+    Color m_MouseOverColor = Color.red;
+    Color m_OriginalColor;
+
+    public GameObject farmLand;
+
+    MeshRenderer m_Renderer;
+
+    void Start()
+    {
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_OriginalColor = m_Renderer.material.color;
+    }
+
+    // Unity functions :3
+    void OnMouseOver()
+    {
+        // Indicates currently selected block by changing color to red
+        m_Renderer.material.color = m_MouseOverColor;
+
+        // Replaces selected block with farmland
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 position = gameObject.transform.position;
+            position.Set(position.x, position.y - 0.1f, position.z);
+            Quaternion rotation = gameObject.transform.rotation;
+
+            Instantiate(farmLand, position, rotation, gameObject.transform.parent);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        m_Renderer.material.color = m_OriginalColor;
+    }
+}
