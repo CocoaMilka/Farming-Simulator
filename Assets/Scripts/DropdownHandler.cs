a489b4aba0352abd326b8cda5312083d60037dd3
Nicholas Pautler <nap44794@uga.edu>
2023-06-30 04:07:05 -0400

    feat: Add template UI with dropdown

---
+++ Assets/Scripts/DropdownHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownHandler : MonoBehaviour
+{
+    public GameObject seedDropdown;
+    public GameObject toolDropdown;
+    public GameObject harvestedFoodDropdown;
+
+    // Called when a dropdown changed uwu like the game
+    public void dropdownChanged()
+    {
+        Debug.Log("Changed");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
