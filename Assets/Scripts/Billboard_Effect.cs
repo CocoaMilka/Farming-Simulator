2c5f944e235ca7b5db747d2673cca5a08985d91d
Nicholas Pautler <nap44794@uga.edu>
2023-07-06 20:24:01 -0400

    feat: Add billboard effect for Jamie ^-^

--- Assets/Scripts/Billboard_Effect.cs
+++ Assets/Scripts/Billboard_Effect.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// This is intended to set a game object's rotation to be the same as the camera's
 public class Billboard_Effect : MonoBehaviour
 {
+    public GameObject camera;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Billboard_Effect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.GetComponent<Transform>().rotation = camera.GetComponent<Transform>().rotation;
     }
 }
