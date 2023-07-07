b1a95cd134220bfca9671fcf182e9756dfdaef57
Cocoa Milka <59587889+CocoaMilka@users.noreply.github.com>
2023-07-07 10:48:28 -0500

    feat: Add player movement

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach this script to Jamie nya :3 this will allow him to move around
+public class PlayerControls : MonoBehaviour
+{
+    
+    private Camera mainCamera;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Stolen from unity docs :3 need to update later for nicer movement
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
